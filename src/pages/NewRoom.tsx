1e1d8771aad2fe0409ac3ebcd6a49b20dbd949c5
Christian-Oliveira <christianoliveirati@gmail.com>
2021-06-22 12:38:16 -0300

    construction: tela de Nova Sala e add navegação

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,18 +1,11 @@
-import {useHistory} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
-import '../styles/auth.scss';
-
 import { Button } from '../components/Button';
 
-export function Home() {
-    const history = useHistory();
+import '../styles/auth.scss';
 
-    function navigateToNewRoom() {
-        history.push('/rooms/new');
-    }
-
+export function NewRoom() {
     return (
         <div id="page-auth">
             <aside>
@@ -24,20 +17,17 @@ export function Home() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Logo Letmeask" />
-                    <button className="create-room" onClick={navigateToNewRoom}>
-                        <img src={googleIconImg} alt="Logo do Google" />
-                        Crie sua sala com o Google
-                    </button>
-                    <div className="separator">ou entre em uma sala</div>
+                    <h2>Criar uma nova sala</h2>
                     <form>
                         <input 
                             type="text"
-                            placeholder="Digite o código da sala"
+                            placeholder="Nome da sala"
                         />
                         <Button type="submit">
-                            Entrar na sala
+                            Criar sala
                         </Button>
                     </form>
+                    <p>Quer entrar em uma sala existente? <Link to="/">clique aqui</Link></p>
                 </div>
             </main>
         </div>
